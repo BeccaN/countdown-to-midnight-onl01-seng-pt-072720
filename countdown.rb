3ad017defe5323f442b77d2c007651579b7f1101
Rebecca Congi <rncongi@gmail.com>
2020-07-30 22:40:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,19 @@
 #write your code here
 
 def countdown(number)
+<<<<<<< HEAD
   until number == 0  
     puts "#{number} SECOND(S)!"
     number -= 1
   end  
   "HAPPY NEW YEAR!"
+=======
+  until number == 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end 
+  puts "HAPPY NEW YEARS!"
+>>>>>>> a08dda2bf84830477e6b21455bb8a3e552343360
 end
 
 def countdown_with_sleep(number)
